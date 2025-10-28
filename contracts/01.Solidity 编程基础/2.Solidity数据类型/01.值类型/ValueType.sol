3e46d8ccda5be45b847f73ec9b29b3ac7c843c83
jioXo <794668063@qq.com>
2025-10-28 21:36:37 +0800

    值类型

---
+++ contracts/01.Solidity 编程基础/2.Solidity数据类型/01.值类型/ValueType.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+/**
+先来介绍值类型，值类型在赋值或传参时，总是进行值拷贝，用值类型声明的变量，总是可以保存在 32 个字节的空间里。
+值类型包含有：
+• 布尔类型（Booleans）
+• 整型（Integers）
+• 定长浮点型（Fixed Point Numbers）
+• 定长字节数组（Fixed-size byte arrays）
+• 有理数和整型常量（Rational and Integer Literals)
+• 字符串常量（String literals）
+• 十六进制常量（Hexadecimal literals）
+• 枚举（Enums）
+• 函数类型（Function Types）
+• 地址类型（Address)
+• 地址常量（Address Literals）
+*/
+contract ValueType {
+
+}
