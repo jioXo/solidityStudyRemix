2735bc347f49e6c3cb6584a8b9e5c61d696cb0f1
jioXo <794668063@qq.com>
2025-10-31 14:13:51 +0800

    优化代码，增加pure

--- contracts/01.Solidity 编程基础/4.变量的作用域和控制结构/TryCatchExample.sol
+++ contracts/01.Solidity 编程基础/4.变量的作用域和控制结构/TryCatchExample.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract TryCatchExample {
-    function tryCatchDemo(address _contractAddress) public returns (bool){
+    function tryCatchDemo(address _contractAddress) public pure  returns (bool){
         // 尝试调用外部合约的函数
         try ExternalContract(_contractAddress).someFunction() returns (bool) {
             // 处理成功
@@ -13,7 +13,7 @@ contract TryCatchExample {
     }
 }
 contract ExternalContract {
-    function someFunction() public returns (bool) {
+    function someFunction() public  pure returns   (bool) {
         // 这里可以是任何逻辑，例如抛出错误
         revert("Error occurred");
     }
