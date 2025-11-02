6dde6ad0b1ebb797bce2d0050f59a02d645ae8a5
jioXo <794668063@qq.com>
2025-11-02 11:13:11 +0800

    地址工具

---
+++ contracts/03.第三章 Solidity 合约与实例/03.地址工具/MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+// import "@openzeppelin/contracts/utils/Address.sol";
+// contract MyToken {
+//     using Address for address;
+//     function send(address recipient, uint256 amount) external {
+//         if (recipient.isContract()) {
+//             // do something
+//         }
+//     }
+// }
