a5715b0f30d05d932be7be655a001846cb6218ec
jioXo <794668063@qq.com>
2025-11-02 09:42:48 +0800

    库的使用

---
+++ contracts/02.第二章 Solidity 高级编程/03.库（Library）的使用/AddTest.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+library SafeMath {
+    function add(uint a, uint b) external  pure returns (uint) {
+        uint c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+}
+// 使用 SafeMath 库的合约
+contract AddTest {
+    function add(uint x, uint y) public pure returns (uint) {
+        return SafeMath.add(x, y);
+    }
+}
